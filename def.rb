d411e219679efdff32b071e8027f8f6a1a918016
Lolly <cclolly@gmail.com>
2014-02-24 01:32:43 +0800

    hi

---
+++ def.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+# -*- coding: UTF-8 -*-
+
+#=============================================================================
+#     FileName: def.rb
+#         Desc:
+#       Author: Lolly
+#        Email: cclolly@gmail.com
+#     HomePage:
+#      Version: 0.0.1
+#   LastChange: 2014-02-24 01:31:26
+#      History:
+#=============================================================================
+
+def talk(any) # => any
+  any
+end
+
+puts talk('hi')
---
+++ hello_world.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+# -*- coding: UTF-8 -*-
+
+#=============================================================================
+#     FileName: hello_world.rb
+#         Desc:
+#       Author: Lolly
+#        Email: cclolly@gmail.com
+#     HomePage:
+#      Version: 0.0.1
+#   LastChange: 2014-02-24 01:30:24
+#      History:
+#=============================================================================
+
+puts 'hi'
