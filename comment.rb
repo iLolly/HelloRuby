9cf5fd41f70efc6b008460f93df4b7a29555dfed
Lolly <cclolly@gmail.com>
2014-02-24 02:26:49 +0800

    add

---
+++ comment.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+# -*- coding: UTF-8 -*-
+
+#=============================================================================
+#     FileName: comment.rb
+#         Desc:
+#       Author: Lolly
+#        Email: cclolly@gmail.com
+#     HomePage:
+#      Version: 0.0.1
+#   LastChange: 2014-02-24 02:24:03
+#      History:
+#=============================================================================
+
+# Comment...
+
+=begin
+嵌入式文档
+block comment...
+=end
