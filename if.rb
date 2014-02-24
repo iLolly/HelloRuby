a32be619e598b85293de151dcdea7b45ddde8d89
Lolly <cclolly@gmail.com>
2014-02-25 00:07:50 +0800

    add

---
+++ if.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+# -*- coding: UTF-8 -*-
+
+#=============================================================================
+#     FileName: if.rb
+#         Desc: 
+#       Author: Lolly
+#        Email: cclolly@gmail.com
+#     HomePage: 
+#      Version: 0.0.1
+#   LastChange: 2014-02-25 00:06:28
+#      History:
+#=============================================================================
+
+if true
+  puts true
+end
