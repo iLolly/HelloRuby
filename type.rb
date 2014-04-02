b745f5ca0d01137937a582130d20573f9c7f48f3
Lolly <lolly@LollydeMacBook-Pro.local>
2014-04-03 01:39:04 +0800

    add

---
+++ type.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+# -*- coding: UTF-8 -*-
+
+#=============================================================================
+#     FileName: type.rb
+#         Desc: 
+#       Author: Lolly
+#        Email: cclolly@gmail.com
+#     HomePage: 
+#      Version: 0.0.1
+#   LastChange: 2014-04-03 01:22:00
+#      History:
+#=============================================================================
+
+x = 'hello'
+if x
+  puts x
+end
